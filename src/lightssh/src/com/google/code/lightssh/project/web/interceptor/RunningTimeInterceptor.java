9e7d622ceaaf91d520b44dcbd0c45997850d3c94
aspen61682@gmail.com <aspen61682@gmail.com@97be41d5-85b2-1997-3cae-9e289364790c>
2012-04-19 12:33:15 +0000

    样式

--- src/lightssh/src/com/google/code/lightssh/project/web/interceptor/RunningTimeInterceptor.java
+++ src/lightssh/src/com/google/code/lightssh/project/web/interceptor/RunningTimeInterceptor.java
@@ -1,8 +1,11 @@
 package com.google.code.lightssh.project.web.interceptor;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.code.lightssh.common.config.SystemConfig;
 import com.google.code.lightssh.common.util.StringUtil;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
@@ -12,13 +15,31 @@ import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
  * @author YangXiaojin
  *
  */
-public class TimeInterceptor extends AbstractInterceptor{
+public class RunningTimeInterceptor extends AbstractInterceptor{
 	
 	private static final long serialVersionUID = 1L;
 	
 	public static final long MAX_EXE_MILLIS = 2000;
 	
-	private static Logger log = LoggerFactory.getLogger(TimeInterceptor.class);
+	private long runningTime = 0;
+	
+	private static Logger log = LoggerFactory.getLogger(RunningTimeInterceptor.class);
+	
+	public static final String RUNNING_TIME_KEY = "log.running.time";
+	
+	@Resource( name="systemConfig" )
+	private SystemConfig systemConfig;
+	
+    public void init() {
+    	if( systemConfig != null && systemConfig.getProperty(RUNNING_TIME_KEY) != null ){
+    		String conf = systemConfig.getProperty(RUNNING_TIME_KEY);
+    		try{
+    			runningTime = Long.valueOf( conf );
+    		}catch( NumberFormatException e ){
+    			log.warn("设置执行时间日志参数[{}]异常：{}",conf,e.getMessage());
+    		}
+    	}
+    }
 	
 	public String intercept(ActionInvocation invocation) throws Exception{
 		long startTime = System.currentTimeMillis(); //开始执行时间
@@ -38,10 +59,14 @@ public class TimeInterceptor extends AbstractInterceptor{
         message.append(invocation.getProxy().getMethod());
         message.append("] 耗时 ").append(executionTime).append(" 毫秒.");
         
-        if( executionTime > MAX_EXE_MILLIS )
+        if( executionTime > getMaxExeMillis() )
         	log.info( message.toString() );
         
 		return result;
+	}
+	
+	protected long getMaxExeMillis( ){
+		return Math.max(this.runningTime,MAX_EXE_MILLIS);
 	}
 
 }
--- src/lightssh/web/pages/home.jsp
+++ src/lightssh/web/pages/home.jsp
@@ -8,12 +8,19 @@
 		<link rel="stylesheet" type="text/css" media="all" href="<%= request.getContextPath() %>/styles/<mys:theme />/theme.css" />
 	</head>
 	
-	<frameset rows="70px,*" frameborder="no" border="0" framespacing="0">
+	<frameset rows="70px,*,20px" frameborder="0" framespacing="0" border="0" >
 		<frame id="head_frame" scrolling="no" src="<s:url value="/header.do"/>"></frame>
-		<frameset id="main_frameset" cols="20%,*">
+		<frameset id="main_frameset" frameborder="0" framespacing="0" border="0" cols="200px,10px,*" width="100%">
 			<frame id="navigation_frame" name="navigation_frame" scrolling="no" src="<s:url value="/navigation.do"/>"></frame>
-			<frame id="main_frame" name="main_frame" scrolling="yes" src="<s:url value="/panel.do"/>"></frame>
+			<frame id="switch_frame" name="switch_frame" scrolling="no" noResize="true" src="<s:url value="/switch.do"/>" />
+			<s:if test="#parameters['url'][0] != null">
+				<frame id="main_frame" name="main_frame" style="margin-left:-5px" scrolling="yes" src="<s:url value="%{#parameters['url'][0]}"/>"></frame>
+			</s:if>
+			<s:else>
+				<frame id="main_frame" name="main_frame" style="margin-left:-15px;" scrolling="yes" src="<s:url value="/panel.do"/>"></frame>
+			</s:else>
 		</frameset>		
+		<frame id="bottom_frame" scrolling="no" src="<s:url value="/footer.do"/>"></frame>
 		<noframes>
 			<body>该浏览器不支持FRAMESET!</body>
 		</noframes>
